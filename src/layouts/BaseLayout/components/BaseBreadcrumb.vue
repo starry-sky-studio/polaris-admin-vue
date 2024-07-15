0aba4788931f964c1bc561460026dfbd4e0b1b86
lvy <upwardsrwr@gmail.com>
2024-07-16 00:33:44 +0800

    fix: router

--- src/layouts/BaseLayout/components/BaseBreadcrumb.vue
+++ src/layouts/BaseLayout/components/BaseBreadcrumb.vue
@@ -1,24 +1,17 @@
 <script setup lang="ts">
 import { myMenuOptions, getMenuItem } from '@/constants'
-import { useRouter, useRoute } from 'vue-router'
+import { useRoute } from 'vue-router'
 
-const router = useRouter()
 const route = useRoute()
 const breadcrumbData = ref<string[]>([])
 
 const handleBreadcrumbData = (path: string) => {
-  // debugger
-
   const pathSplit = path.split('/').filter((i) => i)
   breadcrumbData.value = []
-  console.log(pathSplit)
   pathSplit.forEach((cur: string) => {
     const breadcrumbItem = getMenuItem(cur, myMenuOptions)
-    console.log(breadcrumbItem)
     breadcrumbItem && breadcrumbData.value.push(breadcrumbItem?.label as string)
   })
-
-  console.log(breadcrumbData.value)
 }
 
 watch(
@@ -33,7 +26,6 @@ watch(
 </script>
 
 <template>
-  <Button @click="() => router.push({ name: '404' })">1231</Button>
   <n-breadcrumb separator=">">
     <n-breadcrumb-item
       v-for="item in breadcrumbData"
